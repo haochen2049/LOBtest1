6637ff525139aeff0910d3d74016eb67560b427f
haochen2049 <lihaochen@ymail.com>
2021-09-20 21:47:25 +0100

    1

--- main.py
+++ main.py
@@ -47,17 +47,38 @@ class myWebsocketClient(cbpro.WebsocketClient):
     def on_open(self):
         self.url = "wss://ws-feed.pro.coinbase.com/"
         self.products = ["BTC-USDT"]
-        self.channels=["ticker"]
+        self.channels=["full"]
         self.message_count = 0
     def on_message(self, msg):
         self.message_count += 1
         if (msg["type"] =="received"):
-            print ("Message type:", msg["type"], "\ttrade_id:{:.0f}".format(float(msg["trade_id"]))
-                   , "\tsequence:{:.0f}".format(float(msg["sequence"])), "\ttime:", msg["time"]
-                   , "\tproduct_id:", msg["product_id"], "\tprice:{:.3f}".format(float(msg["price"]))
-                   , "\tside:", msg["side"], "\tlast_size:{:.8f}".format(float(msg["last_size"]))
-                   , "\tbest_bid:{:.0f}".format(float(msg["best_bid"])), "\tbest_ask:{:.0f}".format(float(msg["best_ask"]))
+            print ("Message type:",msg["type"],"\ttime:",msg["time"],
+                   "\tproduct_id:",msg["product_id"],"\tsequence:{:.0f}".format(float(msg["sequence"])),
+                   "\torder_id:",msg["order_id"],"\tsize:{:.8f}".format(float(msg["size"])),
+                   "\tprice:{:.3f}".format(float(msg["price"])),"\tside:",msg["side"],
+                   "\torder_type:",msg["order_type"]
                    )
+        elif (msg["type"] =="open"):
+            print("Message type:", msg["type"]
+                  )
+        elif (msg["type"] =="done"):
+            print("Message type:", msg["type"]
+                  )
+        elif (msg["type"] =="match"):
+            print("Message type:", msg["type"]
+                  )
+        elif (msg["type"] =="change"):
+            print("Message type:", msg["type"]
+                  )
+        elif (msg["type"] =="activate"):
+            print("Message type:", msg["type"]
+                  )
+        elif 'taker_user_id' in msg:
+            print("Message type:", msg["type"]
+                  )
+        else:
+            print(400000004)
+
     def on_close(self):
         print("Closing")
 
