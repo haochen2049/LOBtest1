230f0b7d2ab7f5464d81b4deaec9914a0005e3da
haochen2049 <lihaochen@ymail.com>
2021-09-22 17:07:38 +0100

    1

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
 
-# import cryptofeed
-# from cryptofeed import FeedHandler
-#
-#
-# fh = FeedHandler()
-#
-# # ticker, trade, and book are user defined functions that
-# # will be called when ticker, trade and book updates are received
-# from cryptofeed import FeedHandler
-# from cryptofeed.backends.arctic import FundingArctic, TickerArctic, TradeArctic
-# from cryptofeed.defines import FUNDING, TICKER, TRADES
-# from cryptofeed.exchanges import Bitfinex, Bitmex, Coinbase
-#
-#
-# def main():
-#     f = FeedHandler()
-#     f.add_feed(Coinbase(channels=[TICKER], symbols=['BTC-USD'], callbacks={TICKER: TickerArctic('cryptofeed-test')}))
-#     f.run()
-
-
-
-# const websocket = new CoinbasePro.WebsocketClient(
-#   ["BTC-EUR"],
-#   "wss://ws-feed.pro.coinbase.com",
-#   null, // <-- you need to put your API key in
-#   {
-#     channels: ['ticker']
-#   }
-# );
-# websocket.on('message',data=>data.type==='ticker'&&xPrice=data.price&&console.log(data.price, data))
-#                              // (only want to see ticker messages)
-#                              // you will receive heartbeat (keep-alive) and ticker messages
-#                              // asynchronous callback will send data when it is available
-#                              // you must wait for data to be available and act on it
-
 import time, cbpro
 
 # Simple WebSocket
 wsc = cbpro.WebsocketClient(url="wss://ws-feed.pro.coinbase.com",
                                 products="BTC-USD",
-                                channels=["ticker"])
+                                channels=["full"])
 
 
-# WebSocket Class Example
+# WebSocket Class Example!!
+
 class myWebsocketClient(cbpro.WebsocketClient):
     def on_open(self):
         self.url = "wss://ws-feed.pro.coinbase.com/"
@@ -94,3 +60,6 @@ wsClient.close()
 
 # if __name__ == '__main__':
 #     main()
+
+
+
